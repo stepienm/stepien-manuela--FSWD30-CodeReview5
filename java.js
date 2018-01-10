eaadff95baf47623668657735e2010c5fd788fda
stepien manuela <stepien_manuela@hotmail.com>
2018-01-10 22:32:05 +0100

    my commit

--- java.js
+++ java.js
@@ -1,39 +1,45 @@
-var person0 = {
+var Persons = [{
 			name: "Stefan",
 			surname: "Reitmayer",
 			image: "img/boy1.jpg",  
 			age: 25,
-			like: 7
-		};
-var person1 = {
+			likes: 0,
+					numberlikes: function (i){
+					likes = this.likes +1;
+					document.getElementById('0').innerHTML= likes;
+				}
+			},{
 			name: "Linda",
 			surname: "Winter",
 			image: "img/girl1.jpg",  
 			age: 28,
-			like:5
-		};
-var person2 = {
+			likes: 0,
+					numberlikes: function (i){
+					likes = this.likes +1;
+					document.getElementById('1').innerHTML= likes;
+				}
+			},{
 			name: "Kate",
 			surname: "Whitesteen",
 			image: "img/girl2.jpg",  
 			age: 22,
-			like:7
-		};
-var person3 = {
+			likes: 0,
+					numberlikes: function (i){
+					likes = this.likes +1;
+					document.getElementById('2').innerHTML= likes;
+				}
+			},{
 			name: "Richard",
 			surname: "Cebener",
 			image: "img/boy2.jpg",  
 			age: 27,
-			like: 5
-		};
+			likes: 0,
+					numberlikes: function (i){
+					likes = this.likes +1;
+					document.getElementById('3').innerHTML= likes;
+				}
+			}];
 
-
-	var Persons = [
-		person0, 
-		person1,
-		person2, 
-		person3			
-	];
 
 
 document.write("<br>")
@@ -48,6 +54,7 @@ document.write("<br>")
 document.write("<br>")
 document.write("<br>")
 document.write("<br>")
+
 
 	document.write("<div class='Persons'>");
 	for(var i=0; i<Persons.length;i++) {
@@ -56,9 +63,11 @@ document.write("<br>")
 		document.write("Name: "+Persons[i].name+"<br>");
 		document.write("Surname: "+Persons[i].surname+"<br>");
 		document.write("Age: "+Persons[i].age+"<br>");
-		document.write("<button onclick='return numberoflikes["+i+"]'>Like</button><br>");
+
+		document.write("<button onclick='return numberlikes["+i+"]'>Like</button><br>");
+
 		document.write("<img id='like' src='img/like.png'>");
-		document.write("<p id='demo"+i+" style='color:red'>"+Persons[i].like+"</p><br>");
+		document.write("<p id='"+i+"'style='color:black'>"+Persons[i].likes+"</p><br>");
 		document.write("</div>");
 
 	}
@@ -67,3 +76,4 @@ document.write("<br>")
 
 			
 document.write("</div><br>")
+
